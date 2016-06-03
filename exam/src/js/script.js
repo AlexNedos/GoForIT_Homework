cfe8cc3aa5b0d8795b7b007a8ab6084ed779c39c
Alexandr Nedostupov <anedostupov@mail.ua>
2016-06-03 12:51:14 +0300

    fix margin exam

--- exam/src/js/script.js
+++ exam/src/js/script.js
@@ -1,14 +1,26 @@
 $(function () {
-//		ПЛИТКА КАРТИНОК
+//ISOTOP IMG
 	$('.grid').isotope({
 		itemSelector: '.grid-item',
 		masonry: {
 			columnWidth: 15
 		}
 	});
-
-//	AJAX!!
+//ISOTOP IMG
+//	AJAX
 	var $button = $('.ideas__button');
+	$.ajax({
+		url: 'https://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=games&image_type=photo&pretty=true',
+		success: function (hits) {
+			for (var i = 0; i < 8; i++) {
+				var $ideasBox = $('.ideas__box' + i);
+				var $link = hits.hits[i].webformatURL;
+				var $name = hits.hits[i].tags;
+				var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
+				$ideasBox.html($result);
+			}
+		}
+	});
 	$button.on('click', function () {
 		var $search = $('.ideas__search').val();
 		$.ajax({
@@ -24,7 +36,7 @@ $(function () {
 			}
 		});
 	});
-	
+//	AJAX
 
 	
 	
