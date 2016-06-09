9806e09dfa10f78ae127ad8a5c68645eb6d23b6a
Alexandr Nedostupov <anedostupov@mail.ua>
2016-06-09 04:25:56 +0300

    fix ie8 ajax not ready

--- exam/src/js/script.js
+++ exam/src/js/script.js
@@ -9,33 +9,33 @@ $(function () {
 	//ISOTOP IMG
 	//	AJAX
 	var $button = $('.ideas__button');
-//	$.ajax({
-//		url: 'https://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=blur&image_type=photo&pretty=true',
-//		success: function (hits) {
-//			for (var i = 0; i < 8; i++) {
-//				var $ideasBox = $('.ideas__box' + i);
-//				var $link = hits.hits[i].webformatURL;
-//				var $name = hits.hits[i].tags;
-//				var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
-//				$ideasBox.html($result);
-//			}
-//		}
-//	});
-//	$button.on('click', function () {
-//		var $search = $('.ideas__search').val();
-//		$.ajax({
-//			url: 'https://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=' + $search + '&image_type=photo&pretty=true',
-//			success: function (hits) {
-//				for (var i = 0; i < 8; i++) {
-//					var $ideasBox = $('.ideas__box' + i);
-//					var $link = hits.hits[i].webformatURL;
-//					var $name = hits.hits[i].tags;
-//					var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
-//					$ideasBox.html($result);
-//				}
-//			}
-//		});
-//	});
+	$.ajax({
+		url: 'https://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=blur&image_type=photo&pretty=true',
+		success: function (hits) {
+			for (var i = 0; i < 8; i++) {
+				var $ideasBox = $('.ideas__box' + i);
+				var $link = hits.hits[i].webformatURL;
+				var $name = hits.hits[i].tags;
+				var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
+				$ideasBox.html($result);
+			}
+		}
+	});
+	$button.on('click', function () {
+		var $search = $('.ideas__search').val();
+		$.ajax({
+			url: 'https://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=' + $search + '&image_type=photo&pretty=true',
+			success: function (hits) {
+				for (var i = 0; i < 8; i++) {
+					var $ideasBox = $('.ideas__box' + i);
+					var $link = hits.hits[i].webformatURL;
+					var $name = hits.hits[i].tags;
+					var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
+					$ideasBox.html($result);
+				}
+			}
+		});
+	});
 	//	AJAX
 	//JARUSEL
 	(function ($) {
@@ -58,36 +58,36 @@ $(function () {
 
 //AJAX FOR IE8!!!
 	
-	$button.on('click', function () {
-
-
-	var url = 'http://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=blur&image_type=photo&pretty=true';
-
-	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-
-	var xhr = new XHR();
-	xhr.open('GET', url , true);
-	xhr.send();
-	xhr.onreadystatechange = self.checkResponse = function(event) {
-		if (xhr.readyState != 4) return;
-		if (xhr.status != 200) {
-			console.log(xhr.status + ': ' + xhr.statusText);
-		} else {
-			
-			var imgResponse = JSON.parse(xhr.responseText);
-								for (var i = 0; i < 8; i++) {
-									var $ideasBox = $('.ideas__box' + i);
-									var $link = imgResponse.hits[i].webformatURL;
-									var $name = imgResponse.hits[i].tags;
-									var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
-									$ideasBox.html($result);
-								}
-		}
-	};
-	
-	});
-	
-	
+//	$button.on('click', function () {
+//
+//
+//	var url = 'http://pixabay.com/api/?key=2683000-931909b1d6f29b69f89649b0a&q=blur&image_type=photo&pretty=true';
+//
+//	var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+//
+//	var xhr = new XHR();
+//	xhr.open('GET', url , true);
+//	xhr.send();
+//	xhr.onreadystatechange = self.checkResponse = function(event) {
+//		if (xhr.readyState != 4) return;
+//		if (xhr.status != 200) {
+//			console.log(xhr.status + ': ' + xhr.statusText);
+//		} else {
+//			
+//			var imgResponse = JSON.parse(xhr.responseText);
+//								for (var i = 0; i < 8; i++) {
+//									var $ideasBox = $('.ideas__box' + i);
+//									var $link = imgResponse.hits[i].webformatURL;
+//									var $name = imgResponse.hits[i].tags;
+//									var $result = '<img src="' + $link + '" alt=""> <p>' + $name + '</p>'
+//									$ideasBox.html($result);
+//								}
+//		}
+//	};
+//	
+//	});
+//	
+//	
 	
 	
 	
