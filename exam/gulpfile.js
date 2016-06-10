cda551cd7ebc2e480ce2e725a9ba4114c96ad688
Alexandr Nedostupov <anedostupov@mail.ua>
2016-06-10 22:55:45 +0300

    done exam (and gulp, retina support, sprites

--- exam/gulpfile.js
+++ exam/gulpfile.js
@@ -4,6 +4,8 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var imagemin = require('gulp-imagemin');
+var svgSprite = require('gulp-svg-sprite');
+var sass = require('gulp-sass');
 
 // Concat and minify (js, css)
 gulp.task('minify', function(){
@@ -16,18 +18,47 @@ gulp.task('minify', function(){
 		.pipe(concat('styles.main.min.css'))
 		.pipe(csso())
 		.pipe(gulp.dest('./build'))
-
 });
 
 // minify img
 gulp.task('images', function() {
-	gulp.src('./img/**/*')
+	gulp.src('./img/**/*.png')
 		.pipe(imagemin())
 		.pipe(gulp.dest('./img'))
 
 });
 
+
+config = {
+	shape: {
+		dimension: {
+			maxWidth: 70,
+			maxHeight: 70
+		},
+		spacing: {
+			padding: 0
+		}
+	},
+	mode: {
+		view: {
+			bust: false
+		},
+	}
+};
+gulp.task('svgSprite', function() {
+gulp.src('img/svg/*.svg')
+	.pipe(svgSprite(config))
+	.pipe(gulp.dest('img/'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('src/styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('src/styles/'));
+});
+
+
 gulp.task('default', function() {
-	gulp.run('minify', 'images');
+	gulp.run('minify', 'images', 'svgSprite', 'sass');
 	
 });
